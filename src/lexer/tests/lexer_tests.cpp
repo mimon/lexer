d3bf53d79093537191aa66b3e7d8ab7c12f3b0d7
Simon <simwarg@gmail.com>
2021-02-09 20:35:14 +0100

    Clean up tests

--- src/lexer/tests/lexer_tests.cpp
+++ src/lexer/tests/lexer_tests.cpp
@@ -37,34 +37,51 @@ SCENARIO("When multiple regexs matches a string then multiple nodes are "
       WHEN("Tokenizing the text") {
         lexer.tokenize(text);
 
-        REQUIRE(lexer.nodes.size() == 6); // includes white space
+        REQUIRE(lexer.nodes.size() == 5); // includes white space
 
         THEN("The first token is produced by the second regex because its match "
             "spans over a longer length than the first regex's match") {
           CHECK(lexer.nodes[0].ri == 1);
-        }
+        };
+
 
         THEN("The second token is produced by the first regex because its index "
              "in the token list is lower (higher priority) than the other") {
-          CHECK(lexer.nodes[1].ri == 0);
+          CHECK(lexer.nodes[2].ri == 0);
+        }
+
+        THEN("The fith token is produced by the second regex because its match "
+            "spans over a longer length than the first regex's match") {
+          CHECK(lexer.nodes[4].ri == 1);
         }
       }
     }
+  }
+}
 
-    // WHEN("Evicting all nodes which overlap other nodes") {
-    //     lexer.evict_overlapses();
+SCENARIO("A language with regexs that contain capture sequences should be supported") {
+  GIVEN("A language") {
+    lexer::regex_vector tokens {
+      regex("(\\d+)"),
+      std::regex("[-+]?(\\d*[.])(\\d*)", std::regex::ECMAScript) // float
+    };
 
-    //     THEN("All nodes which overlap another node with higher weight are gone") {
-    //       REQUIRE(lexer.nodes.size() == 3);
-    //       lexer::generic_lexer_node n1 = lexer.nodes[0];
-    //       lexer::generic_lexer_node n2 = lexer.nodes[1];
-    //       lexer::generic_lexer_node n3 = lexer.nodes[2];
+    lexer::generic_lexer lexer(tokens);
 
-    //       CHECK(n1.ri == token::if_statement);
-    //       CHECK(n2.ri == token::whitespace);
-    //       CHECK(n3.ri == token::identifier);
-    //     }
-    //   }
+    GIVEN("A string that contains a float literal") {
+      string text("123.123");
+
+      WHEN("Tokenizing the text") {
+        lexer.tokenize(text);
+
+        REQUIRE(lexer.nodes.size() == 1);
+
+        THEN("The node should span over the entire regex match (and be split by capture sequences)") {
+          CHECK(lexer.nodes[0].p == 0);
+          CHECK(lexer.nodes[0].n == 7);
+        }
+      }
+    }
   }
 }
 
